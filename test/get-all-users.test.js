3b40ef56e9dcb7700270d3a25a4baf54b4a0032e
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2022-12-27 10:42:17 +0530

    latest push

--- test/get-all-users.test.js
+++ test/get-all-users.test.js
@@ -1,112 +1,38 @@
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
-require('../db/database')
-let userOperations = require('../src/operations');
-let userDetailsFixture = {
-                            firstName : "userFirstName",
-                            lastName : "userLastName",
-                            aadharNumber : 1234567890,
-                            phoneNumber : 87900121142
-                        }
+const sinon = require('sinon') 
+const rewire = require('rewire')
+const operations = rewire ('../src/operations')
 
 
-describe('User Operations - Basic flow only:', function() {
-  it('should create user successfully.',async function(done) {
-    let request =  sinon.spy();
-    let response = sinon.spy();
-    	response = {json: sinon.spy(), send: sinon.spy()};
-    	request =  {
-    				"body": userDetailsFixture
-				    }
-        let users = await userOperations.createUserDetails(request, response)
-        console.log(users)
-        expect(users.firstName,userDetailsFixture.firstName)
+
+const reqbody = {
+    firstName : 'dinesh',
+    lastName : 'reddy',
+    aadharNumber : 123,
+    phoneNumber : 2353387583
+}
+
+
+let saveStub  = sinon.stub().returns({reqbody})
+let employee = sinon.stub().returns({save : saveStub})
+
+
+operations.__set__('Employee',employee)
+
+
+describe('testing crud operations with full mocking ',function(){
+    it('it should create one user',function(done){
+        let request = sinon.spy()
+        let response = sinon.spy()
+        response = {
+            json: sinon.spy()
+        }
+        request = {
+            'body' : reqbody
+        }
+        let res = operations.createUserDetails(request,response)
+        console.log(res)
         done()
-    });
-  });
-
-  it('should get details of an user.', function(done){
-    var request =  sinon.spy();
-    var response = sinon.spy();
-        response = {json: sinon.spy(), send: sinon.spy()};
-        request =  {
-                    "params":{
-                            "userId": global.testId
-                        }
-                    }
-    userOperations.getOneUserDetails(request, response).then(function(userDetails){
-        console.log(userDetails)
-        expect(userDetailsFixture.firstName, userDetails.firstName);
-        expect(userDetailsFixture.lastName, userDetails.lastName);
-        expect(userDetailsFixture.aadharNumber, userDetails.aadharNumber);
-        expect(userDetailsFixture.phoneNumber, userDetails.phoneNumber);
-        done();
-    });
-  });
-
-  it('should update user information successfully.', function(done) {
-    var request =  sinon.spy();
-    var response = sinon.spy();
-        response = {json: sinon.spy(), send: sinon.spy()};
-        var _date = new Date();
-        request =  {
-                        "body": {
-                            firstName : "userFirstNameUpdated",
-                            lastName : "userLastNameUpdated",
-                            aadharNumber : 9200123478499,
-                            phoneNumber : 1234,
-                        },
-                        "params":{
-                            "userId": global.testId
-                        }
-                    }
-    userOperations.updateUserDetails(request, response).then(function(userDetails){
-        expect(request.body.firstName, userDetails.firstName);
-        expect(request.body.lastName, userDetails.lastName);
-        expect(request.body.aadharNumber, userDetails.aadharNumber);
-        expect(request.body.phoneNumber, userDetails.phoneNumber);
-        global.testId = userDetails._id;
-        done();
-    });
-  });
-
-  it('should update user information successfully.', function(done) {
-    var request =  sinon.spy();
-    var response = sinon.spy();
-        response = {json: sinon.spy(), send: sinon.spy()};
-        request =  {
-                        "body": {
-                            firstName : "userFirstNameUpdated"
-                        },
-                        "params":{
-                            "userId": global.testId
-                        }
-                    }
-    userOperations.updateUserDetails(request, response).then(function(userDetails){
-        expect(request.body.firstName, userDetails.firstName);
-        expect(userDetailsFixture.lastName, userDetails.lastName);
-        expect(userDetailsFixture.aadharNumber, userDetails.aadharNumber);
-        expect(userDetailsFixture.phoneNumber, userDetails.phoneNumber);
-        global.testId = userDetails._id;
-        done();
-    });
-  });
-
-//   it('should remove details of an user.', function(done){
-//     var request =  sinon.spy();
-//     var response = sinon.spy();
-//         response = {json: sinon.spy(), send: sinon.spy()};
-//         request =  {
-//                     "params":{
-//                             "userId": global.testId
-//                         }
-//                     }
-//     userOperations.removeUserDetails(request, response).then(function(data){
-//         expect(data.status, 200);
-//         done();
-//     });
-//   });
-
-// });
-
+    })
+})
